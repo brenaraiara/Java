87f81296265c71c30b598dfa4292409bb1c3839c
Brena Barradas <brenaraiaracb@gmail.com>
2025-01-01 23:51:52 -0300

    adicionando projeto

---
+++ dia01/ParOuImpar.java
@@ -0,0 +1,20 @@
+package dia01;
+
+    import java.util.Scanner;
+
+    public class ParOuImpar {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+    
+            System.out.print("Digite um numero: ");
+            int num = scanner.nextInt();
+    
+            if (num % 2 == 0) {
+                System.out.println("O numero " + num + " e PAR.");
+            } else {
+                System.out.println("O numero " + num + " e IMPAR.");
+            }
+
+            scanner.close();
+        }
+    }
